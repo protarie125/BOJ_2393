d580d42e35230e871c9ff3ab309e952da7b707d6
protarie <protarie@gmail.com>
2023-05-03 11:56:16 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+using ll = long long;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vl = vector<ll>;
+
+int main() {
+	ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	cout << R"(  ___  ___  ___
+  | |__| |__| |
+  |           |
+   \_________/
+    \_______/
+     |     |
+     |     |
+     |     |
+     |     |
+     |_____|
+  __/       \__
+ /             \
+/_______________\)";
+
+	return 0;
+}
